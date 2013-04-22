9b9587cbf23a3d036f202170b9c14cb76b6aea37
Manjunathappa, Prakash <prakash.pm@ti.com>
2013-04-22 11:57:38 +0530

    for testing only

--- arch/arm/mach-davinci/da850.c
+++ arch/arm/mach-davinci/da850.c
@@ -650,6 +650,7 @@ static const struct mux_config da850_pins[] = {
 	MUX_CFG(DA850, GPIO6_9,		13,	24,	15,	8,	false)
 	MUX_CFG(DA850, GPIO6_10,	13,	20,	15,	8,	false)
 	MUX_CFG(DA850, GPIO6_13,	13,	8,	15,	8,	false)
+	MUX_CFG(DA850, GPIO7_4,		17,	20,	15,	8,	false)
 	MUX_CFG(DA850, RTC_ALARM,	0,	28,	15,	2,	false)
 	/* VPIF Capture */
 	MUX_CFG(DA850, VPIF_DIN0,	15,	4,	15,	1,	false)
