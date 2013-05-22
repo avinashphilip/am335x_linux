1f12fb8f6524ccd726e262a4fcc706e03b0cc15f
KV Sujith <sujithkv@ti.com>
2013-05-22 12:40:27 +0530

    ARM: davinci: da8xx: creation of gpio platform device
    gpio controller resource information being associated with
    davinci_soc_info structure and not created any device. Hence davinci
    gpio didn't fall under proper device model. This patch creates gpio
    davinci as a platform device for da8xx platforms.
    
    - Add Memory and IRQ resources for da8xx
    - Register GPIO platform driver for da8xx.
    - Add da8xx_register_gpio API to create platform device for da8xx
      platforms.
    
    Signed-off-by: KV Sujith <sujithkv@ti.com>
    Signed-off-by: Philip Avinash <avinashphilip@ti.com>
    Signed-off-by: Sekhar Nori <nsekhar@ti.com>

--- arch/arm/mach-davinci/include/mach/da8xx.h
+++ arch/arm/mach-davinci/include/mach/da8xx.h
@@ -96,6 +96,7 @@ int da8xx_register_mmcsd0(struct davinci_mmc_config *config);
 int da850_register_mmcsd1(struct davinci_mmc_config *config);
 void __init da8xx_register_mcasp(int id, struct snd_platform_data *pdata);
 int da8xx_register_rtc(void);
+int da8xx_register_gpio(void *pdata);
 int da850_register_cpufreq(char *async_clk);
 int da8xx_register_cpuidle(void);
 void __iomem * __init da8xx_get_mem_ctlr(void);
